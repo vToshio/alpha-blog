7194f8143aea9fe93d5dc599710a2bab19f41575
Vinícius Toshio <vtoshio2004@gmail.com>
2025-07-13 01:32:30 -0300

    feat: add the new and create routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,22 @@
 class ArticlesController < ApplicationController
   # GET /articles
   def index
-    @articles = Article.all
+    @articles = Article.all.reverse
   end
 
   # GET /articles/:id
   def show
     @article = Article.find(params[:id])
+  end
+
+  # GET /articles/new
+  def new
+  end
+
+  # POST /articles/
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   # Resources
-  resources :articles, only: [ :show, :index ]
+  resources :articles, only: [ :show, :index, :new, :create ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
