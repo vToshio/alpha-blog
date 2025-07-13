2072073190e7d21483a3539c3d229eb4120c43d3
Vinícius Toshio <vtoshio2004@gmail.com>
2025-07-13 01:03:58 -0300

    feat: adding the articles index route

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,9 @@
 class ArticlesController < ApplicationController
+  # GET /articles
+  def index
+    @articles = Article.all
+  end
+
   # GET /articles/:id
   def show
     @article = Article.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   # Resources
-  resources :articles, only: [ :show ]
+  resources :articles, only: [ :show, :index ]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
